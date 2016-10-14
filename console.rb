d4b5f030fa47d3f7982183985623a65052aa83e9
Clacson <cookiellacson@gmail.com>
2016-10-14 13:46:42 +0100

    5 customers initialized in console.rb

--- console.rb
+++ console.rb
@@ -6,3 +6,15 @@ require_relative('models/ticket')
 # Film.delete_all()
 # Customer.delete_all()
 
+customer1 = Customer.new({'name' => 'Cookie', 'funds' => 50})
+customer1.save()
+customer2 = Customer.new({'name' => 'Martin', 'funds' => 60})
+customer2.save()
+customer3 = Customer.new({'name' => 'Roko', 'funds' => 30})
+customer3.save()
+customer4 = Customer.new({'name' => 'Buddy', 'funds' => 70})
+customer4.save()
+customer5 = Customer.new({'name' => 'Dennis', 'funds' => 40})
+customer5.save()
+
+
